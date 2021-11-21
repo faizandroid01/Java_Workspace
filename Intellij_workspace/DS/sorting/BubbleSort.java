cd12a14b820fa9bdbf964a4d841575176dffc1fa
FAIZ <faizrocx9211@gmail.com>
2021-11-21 19:31:19 +0530

    BubbleSort changes - >

--- Intellij_workspace/DS/sorting/BubbleSort.java
+++ Intellij_workspace/DS/sorting/BubbleSort.java
@@ -17,20 +17,20 @@ public class BubbleSort {
         }
         return arr;
     }
-
     public static int[] descSort(int[] arr) {
         int temp;
-        int n = arr.length -1;
-        for (int i = n; i >= 0; i--)
-            for (int j = n; j > n - i; j--)
-                if (arr[j] > arr[j-1]) {
-                   temp = arr[j];
-                   arr[j]= arr[j-1];
-                   arr[j-1]=temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
+            }
+        }
         return arr;
     }
-
+   
     public static void main(String[] args) {
         Arrays.stream(ascSort(new int[]{8, 2, 6, 5, 4, 32, 0, 99, 1})).forEach(x -> System.out.print(x + ","));
         System.out.println();
