63fc377d152075e3306c107deb4099a677ef8968
Faiz Ahmed <faizrocx9211@gmail.com>
2024-07-09 12:46:03 +0530

    neatcode - TUF- COmbination sum - best strategy

--- Intellij_workspace/DS/neatcode/backtrack/CombinationSum.java
+++ Intellij_workspace/DS/neatcode/backtrack/CombinationSum.java
@@ -1,2 +1,73 @@
-package neatcode.backtrack;public class CombinationSum {
+package neatcode.backtrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+/*Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the
+frequency
+ of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+
+
+Constraints:
+
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+All elements of candidates are distinct.
+1 <= target <= 40*/
+
+
+    private static void findValidCombinations(List<List<Integer>> answerList,int[] candidates, int index, int targetSum, List<Integer> integers) {
+
+        if(candidates.length==0)
+            return ;
+
+        if(index == candidates.length){
+            if(targetSum == 0){
+                answerList.add(new ArrayList<>(integers));
+            }
+            return;
+        }
+
+
+        if(candidates[index] <= targetSum ){
+            integers.add(candidates[index]);
+            findValidCombinations(answerList,candidates,index,targetSum-candidates[index],integers);
+            integers.remove(integers.lastIndexOf(candidates[index]));
+        }
+        findValidCombinations(answerList,candidates,index+1,targetSum,integers);
+    }
+
+
+    public static List<List<Integer>> findCombinationSum(int[] candidates, int targetSum) {
+        List<List<Integer>> answerList = new ArrayList<>();
+        findValidCombinations(answerList,candidates,0,targetSum,new ArrayList<>());
+        return answerList;
+    }
+    public static void main(String[] args) {
+        System.out.println(findCombinationSum(new int[]{2,3,6,7},7));
+    }
 }
